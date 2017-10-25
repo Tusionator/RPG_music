0d58b36ea57ccd43e5b86bc7e7b09ee4fd6cb2d2
Tusionator <marta.kowalczyk.93@gmail.com>
2017-10-25 21:49:19 +0200

    Controller name changed

---
+++ src/sample/ButtonsController.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.fxml.FXML;
+
+import java.awt.event.ActionEvent;
+
+public class ButtonsController {
+
+    @FXML
+    public void handleButtonPlay(ActionEvent event){
+        System.out.println("buttonPlay");
+    }
+}
