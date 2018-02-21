a313982884fbea1e3c7a5817729a9e8c2968be9e
Tusionator <marta.kowalczyk.93@gmail.com>
2018-02-21 08:39:22 +0100

    some cleaning

--- src/music/Main.java
+++ src/music/Main.java
@@ -2,19 +2,11 @@ package music;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.HPos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.input.MouseButton;
-import javafx.scene.layout.*;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.util.ArrayList;
-
 public class Main extends Application {
-    private TextArea txtaMusicSongsList;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -22,18 +14,11 @@ public class Main extends Application {
         primaryStage.setTitle(MyStrings.APP_NAME);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
-
-
-//        ManagerFile managerFile = new ManagerFile();
-//        txtaMusicSongsList = (TextArea) root.lookup("#txtaMusicList");
-//        addButtonGrid((GridPane) root.lookup("#gridForButtons"),
-//                managerFile.getSortedMusicFoldersList(), primaryStage);
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
-
 }
 
